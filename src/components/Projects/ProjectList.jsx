ffd2f830c3c3e04dae52a91068bc2089ea27b7d4
amirudheenshareif <ameeraashique6@gmail.com>
2025-05-21 10:31:43 +0530

    Added web-wit project

--- src/components/Projects/ProjectList.jsx
+++ src/components/Projects/ProjectList.jsx
@@ -4,31 +4,31 @@ import Projects from './Projects'
 
 export const ProjectList = () => {
     const projectData = [
-        { 
-          name: 'Portfolio website',
-          githubLink:"https://github.com/amirudheenshareif/Personal-Portfolio",
-          siteLink:"https://amirudheenshareif.github.io/Personal-Portfolio/",
-          imgLink:"https://rngfefhpsfpbbwcrfhze.supabase.co/storage/v1/object/public/company-logo//Portfolio-bg.png" 
-        },
-        { 
-          name: 'Job Portal App',
-          githubLink:"https://github.com/amirudheenshareif/JobVerse",
-          siteLink:"https://job-verse-dusky.vercel.app/",
-          imgLink:"https://rngfefhpsfpbbwcrfhze.supabase.co/storage/v1/object/public/company-logo//landing-page-imgpng.png" 
-        },
-        { 
-          name: 'AI - Data Query Dashboard',
-          githubLink:"https://github.com/amirudheenshareif/Data_Query_Dashboard",
-          siteLink:"https://dashboardassign.vercel.app/",
-          imgLink:"https://rngfefhpsfpbbwcrfhze.supabase.co/storage/v1/object/public/company-logo//dataQueryDashboardBg.png" 
-        },
-        { name: 'Weather Tracker',
-          githubLink:"https://github.com/amirudheenshareif/Dynamic-Weather-Tracker", 
-          siteLink:"https://dynamic-weather-tracker.vercel.app/",
-          imgLink:"https://rngfefhpsfpbbwcrfhze.supabase.co/storage/v1/object/public/company-logo//weather-app.png"
-        },
+      { 
+        name: 'Job Portal App',
+        githubLink:"https://github.com/amirudheenshareif/JobVerse",
+        siteLink:"https://job-verse-dusky.vercel.app/",
+        imgLink:"https://rngfefhpsfpbbwcrfhze.supabase.co/storage/v1/object/public/company-logo//landing-page-imgpng.png" 
+      },
+      { 
+        name: 'AI - Data Query Dashboard',
+        githubLink:"https://github.com/amirudheenshareif/Data_Query_Dashboard",
+        siteLink:"https://dashboardassign.vercel.app/",
+        imgLink:"https://rngfefhpsfpbbwcrfhze.supabase.co/storage/v1/object/public/company-logo//dataQueryDashboardBg.png" 
+      },
+      { name: 'WebWit',
+        githubLink:"https://github.com/amirudheenshareif/WebWit-BlogApp", 
+        siteLink:"https://web-wit-blog-app.vercel.app/https://dynamic-weather-tracker.vercel.app/",
+        imgLink:"https://rngfefhpsfpbbwcrfhze.supabase.co/storage/v1/object/public/company-logo//web-wit-landing-page%20(1).webp"
+      },
+      { 
+        name: 'Portfolio website',
+        githubLink:"https://github.com/amirudheenshareif/Personal-Portfolio",
+        siteLink:"https://amirudheenshareif.github.io/Personal-Portfolio/",
+        imgLink:"https://rngfefhpsfpbbwcrfhze.supabase.co/storage/v1/object/public/company-logo//Portfolio-bg.png" 
+      },
+        
       ];
-      console.log("Hi");
     return(
         <>
         <Projects data={projectData}/>
